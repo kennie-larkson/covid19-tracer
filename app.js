51e7e03525297bcd3de0e4389e2ef6033c245bc1
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-04-05 18:28:58 +0100

    backend data received at frontend and rendered on web page

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-// import { json, response } from "express";
+
+const fetchBackend = async()=>{
+    
+    try{
+        const response = await fetch('http://localhost:4040',{method:'GET',headers:{'Access-Control-Allow-Origin':'*'}})
+        const json = await response.json()
+        // console.log(json)
+    for (const ele of json){
+        
+        const container = document.querySelector('.container')
+        // container.innerHTML = `${datastring}`
+         container.innerHTML = `
+                Total Confirmed: ${ele.confirmed_cases}
+                Recovered: ${ele.recoveries}
+                Critical:${ele.critical_cases}
+                Total Deaths: ${ele.death_cases}
+            ` 
+    }
+}
+catch(err){ console.log(err)} 
+}
 
 
-const loadApp= async()=>{
-    // let element, attached ;
-    // const create = (ele)=>{
-    //      element = document.createElement(ele);
-    //     return element
-    // }
-    // const addEleToBody = (element)=>{
-    //      attached = document.body.append(element);
-    //     return attached
-    // }
+const loadApp=()=>{
 
-    // create('p')
-    // addEleToBody(element)
-    // element.textContent = 'Here\'s #COVID19...'
 
- const response = await fetch('http://localhost:4040')
- const json = await response.json()
- console.log(json)
- .then(console.log('fetch successful'))
- .catch((err)=>{console.log(err)})
+fetchBackend()
     
 }
 
-loadApp()
 
-
-//  document.addEventListener('DOMContentLoaded', loadApp)
+ document.addEventListener('DOMContentLoaded', loadApp)
 
 
 
--- nodeserver.js
+++ nodeserver.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const fetch = require('node-fetch')
 const body_parser = require('body-parser')
-const cors = require('cors')
+const corslib = require('cors')
 const app = express();
 
 // app.use(express.static('public'))
-app.use(body_parser.urlencoded({extended : true}))
-app.use(body_parser.json())
+// app.use(body_parser.urlencoded({extended : true}))
+// app.use(body_parser.json())
 
 //Bring in the CORS library
-app.use(cors({ methods: "GET, POST"}));
+app.use(corslib({ methods: "GET, POST"}));
 
-app.get('/', async (req,res)=>{
-    // res.setHeader('Access-Control-Allow-origin','*');
+let data =[] ,datastring
+const fetchAPI= async()=>{
+    
     var api_options = {
         
         "headers": {
@@ -23,16 +24,38 @@ app.get('/', async (req,res)=>{
     try {
         //retrieving the data from the api server/endpoint
         const response = await fetch("https://covid-19-data.p.rapidapi.com/totals",api_options)
+    //converting the api response to json format
         const json = await response.json()
-         console.log(json)
-        
-         res.send(json)
+    //iterating through the array of objects in the json data using for...of
+    
+        for(const ele of json){
+            datastring = `
+            Total Confirmed: ${ele.confirmed}
+            Recovered: ${ele.recovered}
+            Critical:${ele.critical}
+            Total Deaths: ${ele.deaths}`
+            console.log(datastring);
 
+            data.push({
+            "confirmed_cases":ele.confirmed,
+            "recoveries":ele.recovered,
+            "critical_cases":ele.critical,
+            "death_cases":ele.deaths})
+            
+        }
         
     } catch (error) {
         console.log(error)
         console.log('Oops! Something is amiss...')
     }
+}
+
+
+app.get('/', async (req,res)=>{
+    fetchAPI()
+    
+    res.send(data)
+    
 })
 
 app.listen(4040,()=>{
